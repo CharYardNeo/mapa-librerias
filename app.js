85810ddf090a963c264832eedd7fe188d0694824
CharYardNeo <cquintanasomarriba@gmail.com>
2026-09-12 21:13:23 -0600

    Refactor sedes data and change map provider
    Updated the sedes data structure and modified the map provider to OpenStreetMap.

--- app.js
+++ app.js
@@ -20,10 +20,9 @@ sedes.sort((a, b) => b.porcentaje - a.porcentaje);
 const map = L.map('map', { center: [12.13, -86.17], zoom: 11 });
 
 // NUEVO PROVEEDOR DE MAPA: Estable para archivos locales (CartoDB Positron)
-L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-  subdomains: 'abcd',
-  maxZoom: 20
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
 function crearIcono(sede) {
